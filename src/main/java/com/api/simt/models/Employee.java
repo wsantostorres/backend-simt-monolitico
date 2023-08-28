9e7b22cf2c95250098f94ef48b5bc40db38ee6a3
Wanderson <121998974+wandersonst7@users.noreply.github.com>
2023-08-28 10:35:43 -0300

    Upload de Arquivos Iniciais do Projeto

---
+++ src/main/java/com/api/simt/models/Employee.java
@@ -0,0 +1,8 @@
+package com.api.simt.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Employee {
+}
---
+++ src/main/java/com/api/simt/models/Vacancy.java
@@ -0,0 +1,9 @@
+package com.api.simt.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Vacancy {
+
+}
